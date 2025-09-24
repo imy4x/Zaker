f4edb75540ec880e6f5e73b928cdc09ed35ee499
imy4x <imy4x19072003@gmail.com>
2025-09-24 11:16:08 +0300

    تحدسث 0.4.1

--- lib/models/study_list.dart
+++ lib/models/study_list.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
 
+/// خريطة ثابتة للأيقونات المدعومة
+/// تقدر تضيف أيقونات جديدة هنا إذا احتجت
+const Map<String, IconData> iconMap = {
+  'folder': Icons.folder_special_outlined,
+  'book': Icons.book,
+  'home': Icons.home,
+  'study': Icons.school,
+  'note': Icons.note,
+};
+
 /// نموذج يمثل قائمة أو مجلد لتنظيم جلسات المذاكرة
 class StudyList {
   String id;
   String name;
-  
-  int iconCodePoint;
+  String iconName;
   int colorValue;
 
-  // --- تعديل: تم إصلاح خطأ `const_eval_property_access` ---
-  // تم نقل تعيين القيم الافتراضية إلى قائمة التهيئة الخاصة بالمنشئ
   StudyList({
     required this.id,
     required this.name,
-    int? iconCodePoint,
+    String? iconName,
     int? colorValue,
-  })  : this.iconCodePoint = iconCodePoint ?? Icons.folder_special_outlined.codePoint,
+  })  : this.iconName = iconName ?? 'folder',
         this.colorValue = colorValue ?? Colors.blueGrey.value;
 
+  /// يرجع الأيقونة بناءً على الاسم
+  IconData get icon => iconMap[iconName] ?? Icons.help_outline;
 
-  IconData get icon => IconData(iconCodePoint, fontFamily: 'MaterialIcons');
+  /// يرجع اللون من القيمة
   Color get color => Color(colorValue);
 
+  /// إنشاء كائن من JSON
   factory StudyList.fromJson(Map<String, dynamic> json) {
     return StudyList(
       id: json['id'],
       name: json['name'],
-      iconCodePoint: json['iconCodePoint'] ?? Icons.folder_special_outlined.codePoint,
+      iconName: json['iconName'] ?? 'folder',
       colorValue: json['colorValue'] ?? Colors.blueGrey.value,
     );
   }
 
+  /// تحويل الكائن إلى JSON
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
-      'iconCodePoint': iconCodePoint,
+      'iconName': iconName,
       'colorValue': colorValue,
     };
   }
 }
-
